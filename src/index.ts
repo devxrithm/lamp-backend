8c10db3339e848e9aa2f3135bee15ffe4a474124
itsashkatiyar07 <itsashkatiyar07@gmail.com>
2026-03-16 16:56:51 +0000

    initial setup for lamp-backend

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors'
+
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extends: true }))
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
   res.send(`Hello ${name}!`);
 });
+
+
 
 const port = parseInt(process.env.PORT || '3000');
 app.listen(port, () => {
