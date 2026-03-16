e7d9f84112f913d2239a08a992205a9f4841ebbc
devxrithm <ukcode07@gmail.com>
2026-03-16 17:02:29 +0000

    reFactor: add recommeded packages

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import cors from 'cors'
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+dotenv.config({
+  path: "./.env",
+});
 
 app.use(express.json())
 app.use(express.urlencoded({ extends: true }))
 app.use(cors())
+app.use(cookieParser());
 
-
+//routes
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
   res.send(`Hello ${name}!`);
