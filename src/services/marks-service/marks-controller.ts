5d5d3b1e0d3f7011e9a44cb2ae182886662eb892
Ujjwal Katiyar <ukcode07@gmail.com>
2026-03-17 01:42:43 +0530

    feat: update package.json for new dependencies and scripts; add Prisma integration
    - Changed the dev script to use tsx for better TypeScript support.
    - Added @prisma/adapter-pg and pg as dependencies for PostgreSQL support.
    - Introduced tsx as a dev dependency for TypeScript execution.
    - Created a new prisma.ts file to configure Prisma with PostgreSQL adapter.
    - Updated marks-controller to import prisma from the new lib directory and modified the uploadMarks function to include totalMarks calculation and fixed teamId and teamName assignment.

--- src/services/marks-service/marks-controller.ts
+++ src/services/marks-service/marks-controller.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from "express";
 import { Marks } from "../../types/types";
-import { prisma } from "../../../DB/db-config";
+import { prisma } from "../../lib/prisma";
 
 export const uploadMarks = async (
   req: Request<"", "", Marks>,
@@ -8,7 +8,6 @@ export const uploadMarks = async (
 ) => {
   try {
     const {
-      teamName,
       innovationMarks,
       technicalComplexity,
       presentation,
@@ -20,7 +19,8 @@ export const uploadMarks = async (
 
     await prisma.team.create({
       data: {
-        teamName,
+        teamId: 5214,
+        teamName: "ukcoders-07",
         innovationMarks,
         technicalComplexity,
         presentation,
@@ -28,11 +28,20 @@ export const uploadMarks = async (
         functionality,
         problemRelevance,
         feasibility,
+        totalMarks:
+          innovationMarks +
+          technicalComplexity +
+          presentation +
+          impact +
+          functionality +
+          problemRelevance +
+          feasibility,
       },
     });
 
     res.status(200).json({ message: "Marks uploaded successfully" });
   } catch (error) {
+    console.log("Error uploading marks:", error);
     res.status(400).json({ error: "Invalid request body" });
   }
 };
